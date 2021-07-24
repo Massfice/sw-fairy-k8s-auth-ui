23556d8d96cdd72e5a0d19e8977721c45dd2d60f
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-07-24 23:39:05 +0200

    Fixing error

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,8 @@
 import User from './User';
 import AuthorizeRequestQuery from './AuthorizeRequestQuery';
 import NotifyHandler from './NotifyHandler';
-import OnChangeType from './OnChangeType';
+import _OnChangeType from './OnChangeType';
+
+type OnChangeType = _OnChangeType;
 
 export { User, AuthorizeRequestQuery, NotifyHandler, OnChangeType };
