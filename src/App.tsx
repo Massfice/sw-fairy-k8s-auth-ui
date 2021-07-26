21905371f6683a290fe344b1f691eed579856e62
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-07-27 01:49:44 +0200

    Small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import CounterAppOne from './components/CounterAppOne';
 
-const App = () => (
+const App = (): JSX.Element => (
     <div style={{ margin: '20px' }}>
-        <div>APP-1 - S4 </div>
+        <div>APP-1 - S4</div>
         <div>
             <CounterAppOne />
         </div>
