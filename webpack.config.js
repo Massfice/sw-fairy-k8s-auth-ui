5bae9f06177be29d85a959fc65dabba5495eb828
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-08-23 12:03:29 +0200

    Added server for production

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,59 @@ const { ModuleFederationPlugin } = require('webpack').container;
 const path = require('path');
 const deps = require('./package.json').dependencies;
 
-module.exports = {
-    entry: ['webpack-dev-server/client?http://0.0.0.0:3001', './src/index.ts'],
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 3001,
-        historyApiFallback: true,
-    },
-    output: {
-        publicPath: 'auto',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|tsx|ts)$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'authui',
-            library: { type: 'var', name: 'authui' },
-            filename: 'remoteEntry.js',
-            exposes: {
-                './useAuth': './src/hooks/useAuth',
-                './CallbackPage': './src/pages/CallbackPage',
-            },
-            shared: {
-                ...deps,
-                react: { singleton: true, eager: true, requiredVersion: deps.react },
-                'react-dom': {
-                    singleton: true,
-                    eager: true,
-                    requiredVersion: deps['react-dom'],
+module.exports = (env, { mode }) => {
+    const config = {
+        entry: ['webpack-dev-server/client?http://0.0.0.0:3001', './src/index.ts'],
+        mode: mode || 'development',
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            port: 3001,
+            historyApiFallback: true,
+        },
+        output: {
+            publicPath: 'auto',
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx|tsx|ts)$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
                 },
-            },
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-    ],
+            ],
+        },
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'authui',
+                library: { type: 'var', name: 'authui' },
+                filename: 'remoteEntry.js',
+                exposes: {
+                    './useAuth': './src/hooks/useAuth',
+                    './CallbackPage': './src/pages/CallbackPage',
+                },
+                shared: {
+                    ...deps,
+                    react: { singleton: true, eager: true, requiredVersion: deps.react },
+                    'react-dom': {
+                        singleton: true,
+                        eager: true,
+                        requiredVersion: deps['react-dom'],
+                    },
+                },
+            }),
+        ],
+    };
+
+    if (!mode || mode !== 'production') {
+        config.plugins.push(
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+            }),
+        );
+    }
+
+    return config;
 };
