b7914ff8461f5aef95d6f92f2269bfde40312122
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-08-23 23:43:20 +0200

    Preparing for deployment

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,7 +3,7 @@ import { deployKubernetes, configs } from '@massfice/sw-fairy-common-deploy-scri
 const config = configs.kubernetes(
     __dirname,
     'https://9766b21f-9055-436e-a20a-8018422119a5.k8s.ondigitalocean.com',
-    'sw-fairy-k8s-login-ui',
+    'sw-fairy-k8s-auth-ui',
 );
 
 const deploy = deployKubernetes(config);
